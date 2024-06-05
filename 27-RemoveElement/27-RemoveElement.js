1f50b299453272dc82991ba2dbb99b437a28e4ee
Apirak Fakin <136418361+Apirak19@users.noreply.github.com>
2024-06-05 15:34:39 +0700

    Create file

---
+++ 27-RemoveElement/27-RemoveElement.js
@@ -0,0 +1,19 @@
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    // k = correct length
+    // sorted
+    let i = 0
+/**
+ * @param {number[]} nums
+    for (let j = 0; j<nums.length; j++){
+        if (nums[j] !== val){
+    }
+            nums[i] = nums[j]
+        }
+    
+            i++
+    return i
+};
+[
