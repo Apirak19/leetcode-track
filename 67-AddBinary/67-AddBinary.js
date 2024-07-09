6d1442e6b1324d391565bb2ebe4745d71de21025
Apirak Fakin <136418361+Apirak19@users.noreply.github.com>
2024-07-10 00:28:17 +0700

    Update file 7/10/2024, 12:28:17 AM

--- 67-AddBinary/67-AddBinary.js
+++ 67-AddBinary/67-AddBinary.js
@@ -1,18 +1,18 @@
+const addBinary = (a, b) => {
+    let carry = 0;
+    let result = '';
 
-  for (; len1 >= 0 || len2 >= 0 || carry > 0; len1--, len2--) {
-  let len1 = a.length - 1;
-  let len2 = b.length - 1;
-    let sum = (+a[len1] || 0) + (+b[len2] || 0) + carry;
-    if (sum > 1) {
-      sum = sum % 2;
-      carry = 1;
-    } else {
-  let carry = 0;
-  let result = '';
+    let i = a.length - 1
+    let j = b.length - 1
 
+    for (; i >= 0 || j >= 0 || carry > 0; i--, j--){
+        let sum = (+a[i] || 0) + (+b[j] || 0) + carry
+        if(sum > 1){
+            sum = 0
+        } else {
+            carry = 1
  * @return {string}
  */
-const addBinary = (a, b) => {
 /**
  * @param {string} a
  * @param {string} b
