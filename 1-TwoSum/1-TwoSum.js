0fa4c1d198a729846020a4081ac3797d04c7b33d
Apirak Fakin <136418361+Apirak19@users.noreply.github.com>
2025-05-15 22:50:28 +0700

    Create file

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,16 @@
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++){
+ * @return {number[]}
+ * @param {number} target
+ * @param {number[]} nums
+/**
+        for(let j = i + 1; j < nums.length; j++){
+            if (nums[i] + nums[j] == target){
+        }
+            }
+    }
+                return [i, j]
+
+//  [2,5,5,11]
+};
